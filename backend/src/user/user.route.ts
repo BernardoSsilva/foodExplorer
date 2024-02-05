5fadcb3f62c1998e6e1c5e5d4e813a08b9437407
bernardo.silva <bernardo.silva@agpr5.net>
2024-02-05 11:34:49 -0300

    cannot set headder error fixed

--- backend/src/user/user.route.ts
+++ backend/src/user/user.route.ts
@@ -2,48 +2,51 @@ import { Router } from "express";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 
-
 const userRoute = Router();
-const userService = new UserService()
-const userController = new UserController(userService)
-userRoute.get("/", (req, res) => {
-  const result = userController.getAllUsers()
-  if(!result){
-    res.status(400).send("Bad Request")
+const userService = new UserService();
+const userController = new UserController(userService);
+userRoute.get("/", async (req, res) => {
+  const result = await userController.getAllUsers();
+  if (!result) {
+    res.status(400).json({ message: "Bad request" });
+    return;
   }
-  res.status(200).send(userController.getAllUsers())
+  res.status(200).send(userController.getAllUsers());
 });
 
-userRoute.get("/:id", (req,res)=>{
-  const result = userController.getUserById(req.params.id)
-  if(!result){
-    res.status(400).send("Bad request")
+userRoute.get("/:id", async (req, res) => {
+  const result = await userController.getUserById(req.params.id);
+  if (!result) {
+    res.status(400).json({ message: "Bad request" });
+    return;
   }
-  res.status(200).send(result)
-})
-userRoute.post("/", (req,res) =>{
-  const result = userController.createNewUser(req.body)
-  if(!result){
-    res.status(400).send("Bad request")
+  res.status(200).send(result);
+});
+userRoute.post("/", async (req, res) => {
+  const result = await userController.createNewUser(req.body);
+  if (!result) {
+    res.status(400).json({ message: "Bad request" });
+    return;
   }
-  res.status(201).send(result)
-})
+  res.status(201).send(result);
+});
 
-userRoute.patch("/:id", (req,res) =>{
-  const result = userController.editUser(req.params.id, req.body)
-  if(!result){
-    res.status(400).send("Bad request")
+userRoute.patch("/:id", async (req, res) => {
+  const result = await userController.editUser(req.params.id, req.body);
+  if (!result) {
+    res.status(400).json({ message: "Bad request" });
+    return;
   }
-  res.status(200).send(result)
-})
+  res.status(200).send(result);
+});
 
-userRoute.delete("/:id", (req,res)=>{
-  const result = userController.deleteUser(req.params.id)
-  if(!result){
-    res.status(400).send("Bad Request")
+userRoute.delete("/:id", async (req, res) => {
+  const result = await userController.deleteUser(req.params.id);
+  if (!result) {
+    res.status(400).json({ message: "Bad request" });
+    return;
   }
-  res.status(200).send(result)
-})
-
+  res.status(200).send(result);
+});
 
 export default userRoute;
