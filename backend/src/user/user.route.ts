acb82c07ad8f0bd47db192b5d77ddf2e9d998eee
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-05 15:05:05 -0300

    cleaned code

--- backend/src/user/user.route.ts
+++ backend/src/user/user.route.ts
@@ -50,7 +50,7 @@ userRoute.post("/", (req, res) => {
 userRoute.patch("/", checkToken, async (req, res) => {
   const token = req.headers.authorization?.replace("Bearer ", "") || " ";
 
-  let userId
+  let userId;
   let decoded: DecodedToken;
   try {
     decoded = jwt.verify(token, process.env.mysecret ?? " ") as DecodedToken;
@@ -59,7 +59,7 @@ userRoute.patch("/", checkToken, async (req, res) => {
     res.status(400).send("Bad request");
   }
 
-  const result = await userController.editUser(userId??"", req.body);
+  const result = await userController.editUser(userId ?? "", req.body);
   if (!result) {
     res.status(400).json({ message: "Bad request" });
     return;
@@ -70,7 +70,7 @@ userRoute.patch("/", checkToken, async (req, res) => {
 userRoute.delete("/", checkToken, async (req, res) => {
   const token = req.headers.authorization?.replace("Bearer ", "") || " ";
 
-  let userId
+  let userId;
   let decoded: DecodedToken;
   try {
     decoded = jwt.verify(token, process.env.mysecret ?? " ") as DecodedToken;
@@ -78,7 +78,7 @@ userRoute.delete("/", checkToken, async (req, res) => {
   } catch (err) {
     res.status(400).send("Bad request");
   }
-  const result = await userController.deleteUser(userId??"");
+  const result = await userController.deleteUser(userId ?? "");
   if (!result) {
     res.status(400).json({ message: "Bad request" });
     return;
