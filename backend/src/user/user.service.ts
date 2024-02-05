3f82a3babf1a1b4530466b65608e659d07857624
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-05 10:43:30 -0300

    select user by id and edit routes created

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,7 +4,13 @@ import { CreateUserDto } from "./dtos/create.user.dto";
 export class UserService {
   async findAll() {
     try {
-      const result = await prisma.user.findMany();
+      const result = await prisma.user.findMany({
+        select:{
+            userName:true,
+            userEmail:true,
+            userAdmin:true
+        }
+      });
 
       if (!result) {
         return false;
@@ -13,6 +19,28 @@ export class UserService {
       return result;
     } catch (err) {
       return false;
+    }
+  }
+
+  async findById(userId:string){
+    try{
+        const result = await prisma.user.findUnique({
+            where:{
+                userId
+            },
+            select:{
+                userName:true,
+                userEmail:true,
+                userAdmin:true
+            }
+        })
+
+        if(!result){
+            return false
+        } 
+        return result
+    }catch(err){
+        return false
     }
   }
 
@@ -33,6 +61,22 @@ export class UserService {
       return result
     } catch (err) {
       return false;
+    }
+  }
+
+  async editUser(userId:string, userName:string | undefined, userEmail: string | undefined, userPassword: string | undefined){
+    try{
+        const result = await prisma.user.update({
+            where:{userId},
+            data:{
+                userName,
+                userEmail,
+                userPassword
+            }
+        })
+
+    }catch(err){
+        return false
     }
   }
 }
