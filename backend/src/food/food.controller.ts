58f1598100c6f635818ccac59cba2693b6eed861
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-06 09:09:26 -0300

    created update food route

--- backend/src/food/food.controller.ts
+++ backend/src/food/food.controller.ts
@@ -1,3 +1,4 @@
+import { UpdateFoodDto } from './dto/update.food.dto';
 import { CreateFoodDto } from "./dto/create.food.dto";
 import { FoodService } from "./food.service";
 
@@ -14,5 +15,9 @@ export class FoodController {
 
   async fileUpload(food: number, fileName: string, storedName: string){
     return await this.foodService.fileUpload(food, fileName, storedName)
+  }
+
+  async updateFood(id:number, updateFoodDto:UpdateFoodDto){
+    return await this.foodService.updateFood(id, updateFoodDto)
   }
 }
